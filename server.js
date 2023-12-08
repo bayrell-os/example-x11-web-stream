727435c5222c17d373fc72460a36cd9e4f496c79
Ildar Bikmamatov <ildar@bayrell.org>
2023-12-09 04:16:47 +0600

    some fix

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const App = {
 	web_server: null,
 	web_socket: null,
 	connected_client: null,
+	first_packet: [],
 	
 	onRequest: function (request, response) {
 	
@@ -53,8 +54,18 @@ App.web_socket = new ws.Server({
 App.web_server.on('request', App.onRequest);
 
 App.web_socket.on('connection', (client) => {
+	
 	console.log("User connected");
 	App.connected_client = client;
+	
+	if (App.first_packet.length > 0)
+	{
+		for (let i=0; i<App.first_packet.length; i++)
+		{
+			client.send(App.first_packet[i]);
+		}
+	}
+	
 	client.on('close', () => {
 		App.connected_client = null;
 	});
@@ -66,16 +77,24 @@ App.web_server.listen(8080, () => {
 
 /* Start ffmpeg */
 
-const ffmpeg = require('child_process').spawn("ffmpeg", [
+const ffmpeg_params = [
 	"-re",
-	"-f", "x11grab", "-s", "1440x900", "-i", ":0",
+	"-f", "x11grab", "-s", "1024x768", "-i", ":0",
 	"-framerate", "20",
 	"-tune", "zerolatency",
 	"-preset", "ultrafast",
-	//"-c:v", "libx264",
-	"-f", "mjpeg", "pipe:1"
-	]
-);
+	"-c:v", "libx264", "-b:v", "800k", "-pix_fmt", "yuv420p", //"-s", "1280x720",
+	"-keyint_min", "250",
+	"-movflags", "+frag_keyframe+empty_moov+default_base_moof",
+	"-metadata", "title='media'",
+	//"-f", "mpegts",
+	"-f", "mp4",
+	"pipe:1"
+];
+const ffmpeg = require('child_process').spawn("ffmpeg", ffmpeg_params);
+
+console.log("Run ffmpeg");
+console.log(ffmpeg_params.join(" "));
 
 let connectedClient = null;
 
@@ -93,10 +112,14 @@ ffmpeg.stderr.on('data', function (data) {
 });
 
 ffmpeg.stdout.on('data', function (data) {
+	if (App.first_packet.length < 3)
+	{ 
+		App.first_packet.push(data); 
+	}
 	if (App.connected_client)
 	{
-		let image = new Buffer.from(data).toString('base64');
-		App.connected_client.send(image);
+		//let image = new Buffer.from(data).toString('base64');
+		App.connected_client.send(data);
 	}
 });
 
